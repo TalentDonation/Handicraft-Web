b8ac672e740ab6a9c5c420b0a4a93b8ceb28297a
김상웅 <sangroad123@gmail.com>
2017-06-23 17:13:51 +0900

    add testController

--- handicraft-web/handicraft-api/src/main/java/com/handicraft/api/service/UserServiceImp.java
+++ handicraft-web/handicraft-api/src/main/java/com/handicraft/api/service/UserServiceImp.java
@@ -1,10 +1,9 @@
 package com.handicraft.api.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.handicraft.api.dao.UserDao;
 import com.handicraft.api.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImp implements UserService{
