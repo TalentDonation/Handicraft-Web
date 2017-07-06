b1af471e6343e89368617371fbda3e3ef4d25b59
koseungbin <kosb15@naver.com>
2017-07-06 16:34:17 +0900

    Revert "Revert "Handicraft Api - Swagger library 추가""
    This reverts commit 935ac38d4f6a8aefa0fd28f8a6aa9eb739679122.

--- handicraft-web/handicraft-core/src/main/java/com/handicraft/core/service/UserServiceImp.java
+++ handicraft-web/handicraft-core/src/main/java/com/handicraft/core/service/UserServiceImp.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.handicraft.core.dao.UserDao;
 import com.handicraft.core.dto.User;
 
+import java.util.List;
 
 
 @Service
@@ -13,9 +14,12 @@ public class UserServiceImp implements UserService{
 	
 	@Autowired
 	UserDao userDao;
-	
+
+
 	public User getUser()
 	{
+
 		return userDao.getOne(1);
+
 	}
 }
