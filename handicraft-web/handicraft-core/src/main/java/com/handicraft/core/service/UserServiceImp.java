26ea245779a5931cf9b76b77208687fd2bce8a22
koseungbin <kosb15@naver.com>
2017-07-11 13:39:14 +0900

    handicraft-api update UserController

--- handicraft-web/handicraft-core/src/main/java/com/handicraft/core/service/UserServiceImp.java
+++ handicraft-web/handicraft-core/src/main/java/com/handicraft/core/service/UserServiceImp.java
@@ -1,25 +1,33 @@
 package com.handicraft.core.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.handicraft.core.dao.UserDao;
 import com.handicraft.core.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 @Service
 public class UserServiceImp implements UserService{
-	
+
 	@Autowired
 	UserDao userDao;
 
+	@Override
+	public User getByUser(int u_id) {
+		return userDao.findOne(u_id);
+	}
 
-	public User getUser()
-	{
+	@Override
+	public List<User> getByUserAll() {
+		return userDao.findAll();
+	}
 
-		return userDao.getOne(1);
+	@Override
+	public User insertToUser(User user) {
 
+
+		return userDao.save(user);
 	}
 }
