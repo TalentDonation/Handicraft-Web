2731c648550d2a047c160a245fd608fe84f70145
sangroad <sangroad123@gmail.com>
2017-09-04 20:50:51 +0900

    memberInfo.js 수정

--- handicraft-web/handicraft-admin/src/main/webapp/js/memberInfo.js
+++ handicraft-web/handicraft-admin/src/main/webapp/js/memberInfo.js
@@ -14,12 +14,8 @@ $(document).ready(
             dataType: 'json',
             success: function (response) {
                 $('#panel').show();
-                var data = response.toString();
-                var dataArray = data.split(',');
-
                 emptyTable();
-
-                showData(dataArray);
+                showData(response);
             },
             timeout: 10000,
             error: function (xhr, status, err) {
@@ -35,15 +31,33 @@ function emptyTable(){
     $(".table-tbody").empty();
 }
 
-function showData(arr){
+function showData(data){
     var cnt = 1;
-    // TODO: 표에 나타나야할 개수에 따라 i 바꾸기
-    for (var i = 0; i < arr.length - 2; i += 3) {
+    for(var i = 0; i < data.length; i++){
+
         if(i === 0){
-            $('#table-content').append('<th>No.</th><th>' + arr[i] + '</th><th>' + arr[i + 1] + '</th><th>' + arr[i + 2] + '</th>');
+            var thData = '<th>No.</th>';
+            for(var j = 0; j < data[i].length; j++){
+                thData = thData.concat('<th>',data[i][j],'</th>');
+            }
+
+            $('#table-content').append(thData);
         }
+
         else{
-            $(".table-tbody").append('<tr><td>'+ cnt +'</td><td>' + arr[i] + '</td><td>' + arr[i + 1] + '</td><td>' + arr[i + 2] + '</td></tr>');
+            for(var k = 0; k < data[i].length; k++){
+
+                if(k === 0){
+                    var tdData = '<tr>';
+                    tdData = tdData.concat('<td>', cnt, '</td>');
+                }
+                tdData = tdData.concat('<td>', data[i][k],'</td>');
+
+                if(k === data[i].length - 1){
+                    tdData = tdData.concat('</tr>');
+                    $('.table-tbody').append(tdData);
+                }
+            }
             cnt += 1;
         }
     }
