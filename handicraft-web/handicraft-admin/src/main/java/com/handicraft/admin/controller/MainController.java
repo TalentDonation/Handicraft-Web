71f7a2161d757f5f312988d86469b3bfa5a48f9f
sangroad <sangroad123@gmail.com>
2017-10-11 13:31:17 +0900

    main페이지를 calendar페이지로 변경

--- handicraft-web/handicraft-admin/src/main/java/com/handicraft/admin/controller/MainController.java
+++ handicraft-web/handicraft-admin/src/main/java/com/handicraft/admin/controller/MainController.java
@@ -1,24 +1,14 @@
 package com.handicraft.admin.controller;
 
-import com.handicraft.core.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
-	@Autowired
-	UserService userService;
-
 	@RequestMapping("/")
-	public ModelAndView getMain()
+	public String getMain()
 	{
-
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("main");
-		mv.addObject("user",userService.findByUserAll());
-		return mv;
+		return "redirect:calender";
 	}
 }
