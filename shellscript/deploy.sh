8641dbd6ec543913ee952073316f5e6d16f9de05
Seung Bin . Ko <kosb15@naver.com>
2017-10-24 09:59:39 +0900

    modify dockerfile

--- shellscript/deploy.sh
+++ shellscript/deploy.sh
@@ -9,11 +9,10 @@ function deploy()
  	sh mvnw clean
  	sh mvnw -Dspring.profiles.active="$1-core" install
 
-	if [$2 -eq "api"];
-	 	then
-			echo ---------- build $2 $1 ----------
-		 	cd /app/handicraft-web/handicraft-$2/
-		 	sh mvnw -Dspring.profiles.active="$1-$2" spring-boot:run
+	if [ $2 == "api" ]	;	then
+		echo ---------- build $2 $1 ----------
+		cd /app/handicraft-web/handicraft-$2/
+		sh mvnw -Dspring.profiles.active="$1-$2" spring-boot:run
 	fi
 
 }
